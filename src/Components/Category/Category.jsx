1808d04982e828fbc5ea0f48d5c41f99622a37bc
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2024-08-04 02:45:15 +0530

    Category updated

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -5,11 +5,11 @@ function Category({categorydata}) {
   return (
     <div className='w-36 h-44 sm:w-[350px] cursor-pointer bg-img2 sm:h-[300px] relative overflow-hidden'>
      <div className='w-full h-full flex items-center justify-center'>
-     <img src={categorydata.img} className={` object-cover  ${categorydata.style} transition-all`} />
+     <img src={categorydata.img} className={` object-cover  ${categorydata.style} transition-all rotate-12 hover:rotate-2`} />
      </div>
-     <div className='absolute bottom-0 left-0 h-16 w-full py-2 bg-zinc-800/70 flex flex-col items-center'>
-          <h2 className='text-xl text-zinc-50 font-bold'>{categorydata.title}</h2>
-          <NavLink to={categorydata.url} className='text-sm text-zinc-50'>{categorydata.disc}</NavLink>
+     <div className='absolute bottom-0 left-0  sm:h-16 w-full py-1 sm:py-2 bg-gradient-to-l from-blue-400/40 via-slate-800/50 to-blue-900/40 flex flex-col items-center'>
+          <h2 className='sm:text-xl text-zinc-50 font-bold'>{categorydata.title}</h2>
+          <NavLink to={categorydata.url} className='text-xs sm:text-sm text-zinc-50'>{categorydata.disc}</NavLink>
      </div>
     </div>
   )
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -107,36 +107,36 @@ function Home() {
       <SwiperSlide className=' w-96'><img src={gold_jewelry} className='w-full h-80 sm:h-[80vh] object-cover object-center' /></SwiperSlide>
       <SwiperSlide className=' w-96'><img src={gemstone} className='w-full h-80 sm:h-[80vh] object-cover object-center' /></SwiperSlide>
      </Swiper>
-     <div className='w-full bg-img'>
-          <div className='w-full h-full py-5 flex flex-col gap-3 items-center bg-[#ffffffb4]'>
+     <div className='w-full bg-img-gold'>
+          <div className='w-full h-full py-5 flex flex-col gap-3 items-center bg-[#ffffff6e]'>
                <h2 className='uppercase w-full text-center text-3xl sm:text-4xl py-6 text-zinc-700'>Brides of India</h2>
                <div className='flex items-start justify-center gap-4'>
-                    <div className=' bg-[#ebebeba6] w-[150px] sm:w-80 overflow-hidden'>
+                    <div className=' bg-gradient-to-br from-yellow-600/80 to-orange-500/80 w-[150px] p-1 sm:w-80 overflow-hidden'>
                     <div className='overflow-hidden'>
                          <NavLink to=''>
                               <img src="https://cdn.pixabay.com/photo/2016/10/16/12/28/mehndi-designs-1745048_1280.jpg" className='w-full hover:scale-110 transition-transform h-60 sm:h-[450px] object-cover' />
                          </NavLink>
                     </div>
                          <h2 className=' w-full text-center text-lg text-wrap px-2 font-medium mt-4 '>The Golden Bride</h2>
-                         <p className=' w-full text-center text-sm sm:text-lg text-wrap'>An expression of breathtaking ethnics</p>
+                         <p className=' w-full text-center text-sm sm:text-lg text-wrap'>An expression of breathtaking <br /> ethnics</p>
                     </div>
-                    <div className=' bg-[#f3f3f362] w-[150px] sm:w-80 overflow-hidden'>
+                    <div className='bg-gradient-to-br from-yellow-600/80 to-orange-500/80 w-[150px] p-1 sm:w-80 overflow-hidden'>
                     <div className='overflow-hidden'>
                          <NavLink to=''>
                               <img src="https://cdn.pixabay.com/photo/2017/06/18/14/47/bindi-2416039_1280.jpg" className='w-full hover:scale-110 transition-transform h-60 sm:h-[450px] object-cover' />
                          </NavLink>
                     </div>
                          <h2 className=' w-full text-center text-lg text-wrap px-2 font-medium mt-4 '>The Diamond Bride</h2>
-                         <p className=' w-full text-center text-sm sm:text-lg text-wrap'>A celebration of glam light and shimmer</p>
+                         <p className=' w-full text-center text-sm sm:text-lg text-wrap'>A celebration of glam light and <br /> shimmer</p>
                     </div>
-                    <div className=' bg-[#f3f3f362] w-[150px] sm:w-80 overflow-hidden'>
+                    <div className='bg-gradient-to-br from-yellow-600/80 to-orange-500/80 w-[150px] p-1 sm:w-80 overflow-hidden'>
                     <div className='overflow-hidden'>
                          <NavLink to=''>
                               <img src="https://cdn.pixabay.com/photo/2023/12/19/11/16/red-8457517_1280.jpg" className='w-full hover:scale-110 transition-transform h-60 sm:h-[450px] object-cover' />
                          </NavLink>
                     </div>
                          <h2 className=' w-full text-center text-lg text-wrap px-2 font-medium mt-4 '>The Antique Bride</h2>
-                         <p className=' w-full text-center text-sm sm:text-lg text-wrap'>A treasure of magnificent art and culture</p>
+                         <p className=' w-full text-center text-sm sm:text-lg text-wrap'>A treasure of magnificent art <br /> and culture</p>
                     </div>
                </div>
           </div>
@@ -145,11 +145,12 @@ function Home() {
           <div className='w-32 sm:w-[25%] h-60 sm:h-[580px] '>
                <video src={lftvid} muted autoPlay loop className='w-full h-full object-cover object-center'></video>
           </div>
-          <div className='w-1/2 h-60 sm:h-[580px] bg-zinc-100 flex flex-col gap-[2%] sm:gap-14 items-center'>
-               <h1 className='text-[7vw] sm:text-[5.5vw] uppercase font-medium leading-none pt-5'>Apsara</h1>
-               <p className='Playwrite-AR text-[3vw] sm:text-[2vw] pt-5 leading-none text-center' title='By Vani jewelry'>By Vani jewelry</p>
-               <p className='text-sm sm:text-3xl text-center py-5 sm:pt-10'>A fast moving world of <br /> expression</p>
-               <NavLink to='View collections' className='text-base text-zinc-100 bg-gradient-to-b hover:from-rose-400 hover:to-zinc-800 transition-all from-slate-400 to-zinc-800 uppercase py-1.5 px-3 rounded-md bg-zinc-600'>View collections</NavLink>
+          <div className='w-1/2 h-60 sm:h-[580px] bg-zinc-100 flex flex-col gap-[2%] sm:gap-14 items-center relative'>
+          <div className='absolute w-full h-full opacity-20 bg-img2 z-0'></div>
+               <h1 className='text-[7vw] sm:text-[5.5vw] uppercase relative  font-medium leading-none pt-5'>Apsara</h1>
+               <p className='Playwrite-AR text-[3vw] sm:text-[2vw] relative  pt-5 leading-none text-center' title='By Vani jewelry'>By Vani jewelry</p>
+               <p className='text-sm sm:text-3xl text-center py-5  relative sm:pt-10'>A fast moving world of <br /> expression</p>
+               <NavLink to='View collections' className='text-base relative  text-zinc-100 bg-gradient-to-b hover:from-orange-400 hover:to-zinc-800 transition-all from-slate-400 to-zinc-800 uppercase py-1.5 px-3 rounded-md bg-zinc-600'>View collections</NavLink>
           </div>
           <div className='w-32 sm:w-[25%] h-60 sm:h-[580px] '>
                <video src={rghtvid} muted autoPlay loop className='w-full h-full object-cover object-center'></video>
